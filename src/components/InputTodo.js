33fb7260790e481c6ec8e883d0182880f7e65e45
samuelblebo <samuel.blebo@yahoo.com>
2023-09-18 00:37:44 +0000

    .

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,6 +1,6 @@
 import { React, useState } from "react";
 
-export default function InputTodo() {
+export default function InputTodo({ onTodoAdded }) {
   const [description, setDescription] = useState("");
 
   const onSubmitForm = async (e) => {
@@ -15,12 +15,13 @@ export default function InputTodo() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
       });
-  
+
       if (response.ok) {
         // Clear the form fields
         setDescription("");
         console.log("Posted");
-        // window.location = "/"; // Redirect after successful submission
+        // Call the callback function to notify the parent component about the new todo
+        onTodoAdded();
       } else {
         // Handle errors and provide user feedback
         console.error("Failed to post task");
@@ -32,7 +33,7 @@ export default function InputTodo() {
       // You can display an error message to the user here
     }
   };
-  
+
   return (
     <div className="">
       <h1 className="font-bold text-[24px] mb-[10px] ml-1">Zen Task</h1>
@@ -42,7 +43,7 @@ export default function InputTodo() {
           placeholder="Type your task here"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          className=" w-[200px] lg:w-[280px] xl:w-[400px] h-[45px] lg:h-[50px] px-[30px] rounded-l-lg"
+          className="w-[200px] lg:w-[280px] xl:w-[400px] h-[45px] lg:h-[50px] px-[30px] rounded-l-lg"
         />
         <button
           className="bg-black h-[45px] lg:h-[50px] w-[50px] lg:w-[70px] text-[#fff] text-[25px] rounded-r-lg flex justify-center items-center"
--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -1,19 +1,21 @@
 import { React, useState, useEffect } from "react";
-import EditTodo from "./EditTodo"; // Import the EditTodo component
-
-// icons import
+import EditTodo from "./EditTodo";
 import { BiSolidEditAlt } from "react-icons/bi";
 import { BsCheckCircleFill } from "react-icons/bs";
+import InputTodo from "./InputTodo";
 
 export default function ListTodo() {
   const [todos, setTodos] = useState([]);
+  const [editingTodo, setEditingTodo] = useState(null);
 
-  //delete todo function
   const deleteTodo = async (id) => {
     try {
-      const deleteTodo = await fetch(`https://zen-todo-server.vercel.app/todos/${id}`, {
-        method: "DELETE",
-      });
+      const deleteTodo = await fetch(
+        `https://zen-todo-server.vercel.app/todos/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
 
       setTodos(todos.filter((todo) => todo.todo_id !== id));
     } catch (err) {
@@ -21,60 +23,59 @@ export default function ListTodo() {
     }
   };
 
-  // State to manage the currently editing todo
-  const [editingTodo, setEditingTodo] = useState(null);
-  // Function to update a todo after editing
   const updateTodo = (id, newDescription) => {
     const updatedTodos = todos.map((todo) =>
       todo.todo_id === id ? { ...todo, description: newDescription } : todo
     );
     setTodos(updatedTodos);
-    setEditingTodo(null); // Hide the EditTodo component
+    setEditingTodo(null);
   };
 
-  // get todos function
   const getTodos = async () => {
     try {
       const response = await fetch("https://zen-todo-server.vercel.app/todos", {
         method: "GET",
       });
       const jsonData = await response.json();
-  
+
       setTodos(jsonData);
     } catch (err) {
       console.error(err.message);
     }
   };
-  
 
   useEffect(() => {
     getTodos();
   }, []);
 
+  const handleTodoAdded = () => {
+    // When a new todo is added, we can simply fetch the updated list of todos
+    getTodos();
+  };
+
   return (
     <div className="overflow-x-auto no-scrollbar h-[450px]">
-      <table class="table mt-5  ">
+      <InputTodo onTodoAdded={handleTodoAdded} />
+      <table className="table mt-5">
         <tbody>
           {todos.map((todo) => (
             <tr
               key={todo.todo_id}
-              className=" flex items-center justify-center mb-2 lg:mb-4 "
+              className="flex items-center justify-center mb-2 lg:mb-4"
             >
-              <td className=" w-[180px] lg:w-[260px] xl:w-[380px] bg-green-300  h-[40px]  lg:h-[50px] flex items-center rounded-l-lg px-[25px]">
+              <td className="w-[180px] lg:w-[260px] xl:w-[380px] bg-green-300 h-[40px] lg:h-[50px] flex items-center rounded-l-lg px-[25px]">
                 {editingTodo === todo.todo_id ? (
                   <EditTodo todo={todo} onUpdate={updateTodo} />
                 ) : (
                   todo.description
                 )}
               </td>
-              <td className="bg-[#000] text-[#fff] h-[40px]  lg:h-[50px] w-[30px] flex items-center justify-center">
-                {editingTodo === todo.todo_id ? null : ( // No "Edit" button while editing
-                  <BiSolidEditAlt
-                    onClick={() => setEditingTodo(todo.todo_id)}
-                  />
+              <td className="bg-[#000] text-[#fff] h-[40px] lg:h-[50px] w-[30px] flex items-center justify-center">
+                {editingTodo === todo.todo_id ? null : (
+                  <BiSolidEditAlt onClick={() => setEditingTodo(todo.todo_id)} />
                 )}
               </td>
-              <td className="bg-[#000] text-green-300 w-[35px] h-[40px]  lg:h-[50px] flex items-center justify-center rounded-r-lg">
+              <td className="bg-[#000] text-green-300 w-[35px] h-[40px] lg:h-[50px] flex items-center justify-center rounded-r-lg">
                 <BsCheckCircleFill onClick={() => deleteTodo(todo.todo_id)} />
               </td>
             </tr>
