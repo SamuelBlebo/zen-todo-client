57c2ebb73389c3e5ba2c589806826d56d904df57
samuelblebo <samuel.blebo@yahoo.com>
2023-09-18 00:57:36 +0000

    .

--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -54,7 +54,7 @@ export default function ListTodo() {
   };
 
   return (
-    <div className="overflow-x-auto no-scrollbar h-[450px] lg:h-[600px]">
+    <div className="overflow-x-auto no-scrollbar h-[450px] lg:h-[600px] relative">
       <InputTodo onTodoAdded={handleTodoAdded} />
       <table className="table mt-5">
         <tbody>
